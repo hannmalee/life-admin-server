eee89fdb8d41233ce825b7f4aa313205894197a4
hann <hmweeks93@gmail.com>
2021-09-21 11:43:52 -0500

    updates to auth.py

--- lifeadminapi/views/auth.py
+++ lifeadminapi/views/auth.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from lifeadminapi.models import HouseholdUser
+from lifeadminapi.models import Household
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -53,9 +54,14 @@ def register_user(request):
         last_name=request.data['last_name']
     )
 
+    household = Household.objects.create(
+        name=request.data['household_name'],
+        description=request.data['description']
+        )
+
     # Now save the extra info in the levelupapi_gamer table
     household_user = HouseholdUser.objects.create(
-        household=request.data['household'],
+        household=household,
         user=new_user
     )
 
